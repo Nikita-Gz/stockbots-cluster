28a5bb7635fee00853a3bbfe856e68b51adc2765
Nikita Gezha <ngez.work@gmail.com>
2022-12-16 14:01:35 +0200

    mlflow tracking server setup

---
+++ deploy_mlflow_server.sh
@@ -0,0 +1,10 @@
+set -e
+version=v0.$(date +%s)
+echo "Building with version $version"
+docker build . -f Dockerfile_mlflow_server -t data6s/mlflow-server -t data6s/mlflow-server:$version
+docker push data6s/mlflow-server:$version
+docker push data6s/mlflow-server:latest
+kubectl apply -f ./mlflow-tracking/pv-pvc.yaml
+kubectl apply -f ./mlflow-tracking/mlflow-config.yaml
+kubectl apply -f ./mlflow-tracking/service.yaml
+kubectl replace -f ./mlflow-tracking/deployment.yaml --force
